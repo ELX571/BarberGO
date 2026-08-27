75c0b665737fcc0006e7592816b7e2a68ddacabd
ELX571 <20saidazimxon07@gmail.com>
2026-08-27 20:08:25 +0500

    design ozgardi post searching qoshildi

--- test_search.py
+++
@@ -1,8 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-
-from chat.models import ChatRoom
-from django.db.models import Q
-qs = ChatRoom.objects.all()
-print("All rooms:", qs.count())
